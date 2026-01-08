a3ea19529a166cc1f516dc712d7a7c86adc149a5
RostislavStaffs <v015989p@student.staffs.ac.uk>
2026-01-08 15:18:36 +0000

    admin login now loads admin dashboard
    admin login now loads admin dashboard

--- routes/adminEventsService.js
+++ routes/adminEventsService.js
@@ -1,10 +1,15 @@
 import express from "express";
+import { requireAuth } from "../middleware/authMiddleware.js";
+import { requireAdmin } from "../middleware/adminMiddleware.js";
+
 const router = express.Router();
 
-//POST SINGLE Event
-router.post("/:id", (req, res) => {
-  res.json({ message: "Post a single event" });
-});
+// protect everything below
+router.use(requireAuth, requireAdmin);
 
+// POST SINGLE Event
+router.post("/:id", (req, res) => {
+  res.json({ message: "Post a single event (admin only)" });
+});
 
 export default router;
